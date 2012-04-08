c9a05fa55e2d37ed0520391ac16aa6f9002072bc
Tristan O'Neil <tristanoneil@gmail.com>
2012-04-08 11:39:39 -0400

    Updated database.

--- lib/marked.rb
+++ lib/marked.rb
@@ -5,7 +5,7 @@ require "mongo_mapper"
 require "redcarpet"
 
 MongoMapper.connection = Mongo::Connection.new("staff.mongohq.com", 10082, :pool_size => 5, :pool_timeout => 5)
-MongoMapper.database   = "grst"
+MongoMapper.database   = "marked"
 MongoMapper.database.authenticate(ENV["MONGO_USERNAME"], ENV["MONGO_PASSWORD"])
 
 require "marked/app"
